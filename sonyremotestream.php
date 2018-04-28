3f8447503f39803ca44180af504cd1df48324847
Steve Bennett <steve@electricpocket.com>
2018-04-28 12:42:45 +0100

    g

--- sonyremotestream.php
+++ sonyremotestream.php
@@ -224,30 +224,34 @@ function fresh() {
 			error_log(date('Y-m-d H:i:s')." Read stream length: ".strlen($part)."\n", 3, 'streamerror.log');
 			if (strstr($part, '--' . $boundary)) {
 				$in = true;
-				error_log(date('Y-m-d H:i:s')." Found boundary:".$part."/n".bin2hex($part)."\n", 3, 'streamerror.log');
+				error_log(date('Y-m-d H:i:s')." Found boundary:".$part."\n".bin2hex($part)."\n", 3, 'streamerror.log');
 			}
-			if (strstr($part,"Content-Length")) {
 			
-				error_log(date('Y-m-d H:i:s')." Length:".$part."/n".bin2hex($part)."\n", 3, 'streamerror.log');
-			}
 			$buffer .= $part;
+			if (strstr($part,"\r\n\r\n")) {
+					
+				error_log(date('Y-m-d H:i:s')." Headers :".$buffer."\n".bin2hex($part)."\n", 3, 'streamerror.log');
+			}
 			$part = $buffer;
 			error_log(date('Y-m-d H:i:s')."part len before cleaning ".strlen($part)."\n", 3, 'streamerror.log');
-			if (substr(trim($part), 0, 2) == "--")
-				$part = substr($part, 3);
+			$part = trim(substr($part, strpos($part, "\r\n\r\n")));
+			error_log(date('Y-m-d H:i:s')."part len after remove http headers ".strlen($part)."\n", 3, 'streamerror.log');
+				
+			//if (substr(trim($part), 0, 2) == "--")
+			//	$part = substr($part, 3);
 			$part = substr($part,
 					strpos($part, '--' . $boundary) + strlen('--' . $boundary));
 			error_log(date('Y-m-d H:i:s')."part len after remove first boundary ".strlen($part)."\n", 3, 'streamerror.log');
-			$part = trim(substr($part, strpos($part, "\r\n\r\n")));
-			error_log(date('Y-m-d H:i:s')."part len after remove http headers ".strlen($part)."\n", 3, 'streamerror.log');
+			
 			if (strstr($part, '--' . $boundary)) {
 				
 				error_log(date('Y-m-d H:i:s')." Found end boundary:".strlen($part)."at pos ".strpos($part, '--' . $boundary)."\n", 3, 'streamerror.log');
 			}
 			$part = substr($part, 0, strpos($part, '--' . $boundary));
+			if (strlen($part)>0) file_put_contents("newimage.jpg",$buffer);
 			error_log(date('Y-m-d H:i:s')."part len after looking for next boundary ".strlen($part)."\n", 3, 'streamerror.log');
 			error_log(date('Y-m-d H:i:s')." attempting image creation len ".strlen($part)."\n", 3, 'streamerror.log');
-			//file_put_contents("newimage.jpg",$buffer);
+			
 			$img = @imagecreatefromstring($part);
 			if ($img) {
 				error_log(date('Y-m-d H:i:s')." got stream image:\n", 3, 'streamerror.log');
