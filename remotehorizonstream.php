415b32084e3d07481b4d319b67a16931626dbfa6
steve <steve@pocketmariner.com>
2019-10-04 10:24:47 +0100

    put 7185 back to mjpeg feed

--- remotehorizonstream.php
+++ remotehorizonstream.php
@@ -33,7 +33,7 @@ $port = $_GET['port'] + 10000;//"17125";
 $url = "/?action=stream";
 
 $sonyrtsp=false;
-if ($_GET['port']==7156 || $_GET['port']==7185 || $_GET['port']==7190 || $_GET['port']==7191 || $_GET['port']==7192 || $_GET['port']==7193 || $_GET['port']==7186 || $_GET['port']==7209 ) 
+if ($_GET['port']==7156 || $_GET['port']==7190 || $_GET['port']==7191 || $_GET['port']==7192 || $_GET['port']==7193 || $_GET['port']==7186 || $_GET['port']==7209 ) 
 {
 	$sonyrtsp=true;//special case for sony cameras we use cvlc to read rtsp stream from 10.106.16.84/media/video2
 }
@@ -48,8 +48,9 @@ if ($sonyrtsp) //special case for sony cameras we use cvlc to read rtsp stream f
 {
     $url = "/webcam";
     $boundary = "7b3cc56e5f51db803f790dad720ed50a";
+    
 }
-if (false)//$_GET['port']==7185)
+if ($_GET['port']==7185)
 {
     $url = "/cgi-bin/mjpeg?resolution=400x300";
     $boundary = "myboundary";
@@ -192,7 +193,7 @@ function fresh() {
 	if ($fp) {
 		$username = "fleetrange";
 		$password = trim($port - 10000);
-		if (false) //$_GET['port']==7185)
+		if ($_GET['port']==7185)
 		{
 		    $username = "user1";
 		    $password = "Tallink1";
@@ -219,7 +220,7 @@ function fresh() {
 			//if they erroneously put the leading -- in the Content-type header we need to clobber it so it doesn't
 			//mess up our boundary parsing/substring malarchy
 			//Content-type: multipart/x-mixed-replace;boundary =--myboundary
-			if (false)//$_GET['port']==7185)
+			if ($_GET['port']==7185)
 			{ 
 			    if (strpos($part,"boundary =--"))
     			{
